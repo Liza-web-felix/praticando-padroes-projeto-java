7a1edc45bf2c04e9a62fc6dca6af43e22986b126
Liza-web-felix <lizfelix.ti@outlook.com>
2022-11-28 13:15:54 -0300

    projeto adicionado

---
+++ praticando-padroes-projeto-spring/src/test/java/desafio/springdigitalinnovation/gof/PraticandoPadroesProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package desafio.springdigitalinnovation.gof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticandoPadroesProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
